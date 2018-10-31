4790ddcd35ab89d62bdb93272cd3199e477e2d8e
qupan <34915305+qupan@users.noreply.github.com>
2018-10-31 14:42:10 +0800

    Update Excel_Report -demo.py

--- word/Excel_Report -demo.py
+++ word/Excel_Report -demo.py
@@ -9,6 +9,7 @@ from openpyxl.styles import colors
 from openpyxl.styles import Fill,fills
 from openpyxl.formatting.rule import ColorScaleRule
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 try:
 	import sys
 	reload(sys)
@@ -186,7 +187,15 @@ class Excel_Report():
 
 	def total_time(self):
 		#得到运行总时间
-		driver=webdriver.Chrome()
+		chrome_options=webdriver.ChromeOptions()
+		chrome_options.add_argument('--no-sandbox')#解决DevToolsActivePort文件不存在的报错
+		chrome_options.add_argument('--headless')#浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+		chrome_options.add_argument('--disable-gpu')#谷歌文档提到需要加上这个属性来规避bug
+		chrome_options.add_argument('--window-size=1920,1080')#指定浏览器分辨率
+		chrome_options.add_argument('blink-settings=imagesEnabled=false') #不加载图片, 提升速度
+		chrome_options.add_argument('--hide-scrollbars') #隐藏滚动条, 应对一些特殊页面
+		#chrome_options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" #手动指定使用的浏览器位置
+		driver=webdriver.Chrome(chrome_options=chrome_options)
 		driver.get(self.html_addr)
 		time.sleep(3)
 		try:
